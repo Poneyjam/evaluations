56788269d2ec7f49f2d39697da1de52afe4f8b3a
Cookah <alexis.poncet.pro@gmail.com>
2025-06-07 14:02:45 +0200

    process.env.PORT ajouté

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // autoriser la visualisation de l'IP des ordinateurs
 app.set('trust proxy', true);
